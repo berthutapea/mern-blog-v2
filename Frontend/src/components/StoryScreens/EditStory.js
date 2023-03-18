555655825ee6fb279ff01b818872bd527300be03
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-18 21:46:39 +0700

    No Stories

--- Frontend/src/components/StoryScreens/EditStory.js
+++ Frontend/src/components/StoryScreens/EditStory.js
@@ -108,7 +108,7 @@ const EditStory = () => {
                                 <div class="absolute">
                                     Currently Image
                                 </div>
-                                <img src={`http://localhost:5000/storyImages/${previousImage}`} alt="storyImage" />
+                                <img src={`https://api.blog.msnproduction.com/storyImages/${previousImage}`} alt="storyImage" />
                             </div>
                             <div class="StoryImageField">
                                 <AiOutlineUpload />

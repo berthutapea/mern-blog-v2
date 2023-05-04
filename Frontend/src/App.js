fd9feec549468197acdd8974bc90dafb99e2b5b1
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-05-04 23:24:38 +0700

    Refactor

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,103 +1,103 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Outlet } from 'react-router-dom'
-import PrivateRoute from './components/Routing/PrivateRoute';
-import Home from "./components/GeneralScreens/Home"
-import LoginScreen from "./components/AuthScreens/LoginScreen"
-import RegisterScreen from "./components/AuthScreens/RegisterScreen"
-import ForgotPasswordScreen from "./components/AuthScreens/ForgotPasswordScreen"
-import ResetPasswordScreen from "./components/AuthScreens/ResetPasswordScreen"
-import AddStory from './components/StoryScreens/AddStory';
-import DetailStory from './components/StoryScreens/DetailStory';
-import Header from './components/GeneralScreens/Header';
-import Footer from './components/GeneralScreens/Footer';
-import Profile from './components/ProfileScreens/Profile';
-import EditProfile from './components/ProfileScreens/EditProfile';
-import ChangePassword from './components/ProfileScreens/ChangePassword';
-import NotFound from './components/GeneralScreens/NotFound';
-import EditStory from './components/StoryScreens/EditStory';
-import ReadListPage from './components/ProfileScreens/ReadListPage';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import { Outlet } from 'react-router-dom'
+// import PrivateRoute from './components/Routing/PrivateRoute';
+// import Home from "./components/GeneralScreens/Home"
+// import LoginScreen from "./components/AuthScreens/LoginScreen"
+// import RegisterScreen from "./components/AuthScreens/RegisterScreen"
+// import ForgotPasswordScreen from "./components/AuthScreens/ForgotPasswordScreen"
+// import ResetPasswordScreen from "./components/AuthScreens/ResetPasswordScreen"
+// import AddStory from './components/StoryScreens/AddStory';
+// import DetailStory from './components/StoryScreens/DetailStory';
+// import Header from './components/GeneralScreens/Header';
+// import Footer from './components/GeneralScreens/Footer';
+// import Profile from './components/ProfileScreens/Profile';
+// import EditProfile from './components/ProfileScreens/EditProfile';
+// import ChangePassword from './components/ProfileScreens/ChangePassword';
+// import NotFound from './components/GeneralScreens/NotFound';
+// import EditStory from './components/StoryScreens/EditStory';
+// import ReadListPage from './components/ProfileScreens/ReadListPage';
 
-const App = () => {
+// const App = () => {
 
-      return (
-            <Router>
+//       return (
+//             <Router>
 
-                  <div className="App">
+//                   <div className="App">
 
-                        <Routes>
-                              <Route path="/" element={<LayoutsWithHeader />}>
+//                         <Routes>
+//                               <Route path="/" element={<LayoutsWithHeader />}>
 
-                                    <Route path='*' element={<NotFound />} />
+//                                     <Route path='*' element={<NotFound />} />
 
-                                    <Route exact path='/' element={<PrivateRoute />}>
-                                          <Route exact path='/' element={<Home />} />
-                                    </Route>
+//                                     <Route exact path='/' element={<PrivateRoute />}>
+//                                           <Route exact path='/' element={<Home />} />
+//                                     </Route>
 
-                                    <Route exact path="/story/:slug" element={<DetailStory />} />
+//                                     <Route exact path="/story/:slug" element={<DetailStory />} />
 
-                                    <Route exact path='/addstory' element={<PrivateRoute />}>
-                                          <Route exact path='/addstory' element={<AddStory />} />
-                                    </Route>
+//                                     <Route exact path='/addstory' element={<PrivateRoute />}>
+//                                           <Route exact path='/addstory' element={<AddStory />} />
+//                                     </Route>
 
-                                    <Route exact path='/profile' element={<PrivateRoute />}>
-                                          <Route exact path='/profile' element={<Profile />} />
-                                    </Route>
+//                                     <Route exact path='/profile' element={<PrivateRoute />}>
+//                                           <Route exact path='/profile' element={<Profile />} />
+//                                     </Route>
 
-                                    <Route exact path='/edit_profile' element={<PrivateRoute />}>
-                                          <Route exact path='/edit_profile' element={<EditProfile />} />
-                                    </Route>
+//                                     <Route exact path='/edit_profile' element={<PrivateRoute />}>
+//                                           <Route exact path='/edit_profile' element={<EditProfile />} />
+//                                     </Route>
 
-                                    <Route exact path='/change_Password' element={<PrivateRoute />}>
-                                          <Route exact path='/change_Password' element={<ChangePassword />} />
-                                    </Route>
+//                                     <Route exact path='/change_Password' element={<PrivateRoute />}>
+//                                           <Route exact path='/change_Password' element={<ChangePassword />} />
+//                                     </Route>
 
-                                    <Route exact path='/story/:slug/like' element={<PrivateRoute />}>
-                                          <Route exact path='/story/:slug/like' element={<DetailStory />} />
-                                    </Route>
+//                                     <Route exact path='/story/:slug/like' element={<PrivateRoute />}>
+//                                           <Route exact path='/story/:slug/like' element={<DetailStory />} />
+//                                     </Route>
 
-                                    <Route exact path='/story/:slug/edit' element={<PrivateRoute />}>
-                                          <Route exact path='/story/:slug/edit' element={<EditStory />} />
-                                    </Route>
+//                                     <Route exact path='/story/:slug/edit' element={<PrivateRoute />}>
+//                                           <Route exact path='/story/:slug/edit' element={<EditStory />} />
+//                                     </Route>
 
-                                    <Route exact path='/story/:slug/delete' element={<PrivateRoute />}>
-                                          <Route exact path='/story/:slug/delete' element={<DetailStory />} />
-                                    </Route>
-                                    <Route exact path='/story/:slug/addComment' element={<PrivateRoute />}>
-                                          <Route exact path='/story/:slug/addComment' element={<DetailStory />} />
-                                    </Route>
+//                                     <Route exact path='/story/:slug/delete' element={<PrivateRoute />}>
+//                                           <Route exact path='/story/:slug/delete' element={<DetailStory />} />
+//                                     </Route>
+//                                     <Route exact path='/story/:slug/addComment' element={<PrivateRoute />}>
+//                                           <Route exact path='/story/:slug/addComment' element={<DetailStory />} />
+//                                     </Route>
 
-                                    <Route exact path='/readList' element={<PrivateRoute />}>
-                                          <Route exact path='/readList' element={<ReadListPage />} />
-                                    </Route>
+//                                     <Route exact path='/readList' element={<PrivateRoute />}>
+//                                           <Route exact path='/readList' element={<ReadListPage />} />
+//                                     </Route>
 
-                              </Route>
+//                               </Route>
 
-                              <Route exact path="/login" element={<LoginScreen />} />
-                              <Route exact path="/register" element={<RegisterScreen />} />
+//                               <Route exact path="/login" element={<LoginScreen />} />
+//                               <Route exact path="/register" element={<RegisterScreen />} />
 
-                              <Route exact path="/forgotpassword" element={<ForgotPasswordScreen />} />
+//                               <Route exact path="/forgotpassword" element={<ForgotPasswordScreen />} />
 
-                              <Route exact path="/resetpassword" element={<ResetPasswordScreen />} />
+//                               <Route exact path="/resetpassword" element={<ResetPasswordScreen />} />
 
 
-                        </Routes>
+//                         </Routes>
 
-                  </div>
+//                   </div>
 
-            </Router>
+//             </Router>
 
-      );
+//       );
 
-}
+// }
 
-const LayoutsWithHeader = () => {
-      return (
-            <>
-                  <Header />
-                  <Outlet />
-                  <Footer />
-            </>
-      );
-}
+// const LayoutsWithHeader = () => {
+//       return (
+//             <>
+//                   <Header />
+//                   <Outlet />
+//                   <Footer />
+//             </>
+//       );
+// }
 
-export default App;
+// export default App;

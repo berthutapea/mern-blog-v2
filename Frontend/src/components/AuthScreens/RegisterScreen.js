8108191293bee337b1d0b7771133c2d2cb5a4434
hperedo <hperedo1@yahoo.com.mx>
2023-05-22 10:13:43 +0200

    fix_user_to_ocovert_other_users_data

--- Frontend/src/components/AuthScreens/RegisterScreen.js
+++ Frontend/src/components/AuthScreens/RegisterScreen.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "../../Css/Register.css"
+import "../../Css/Register.css";
 const RegisterScreen = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
@@ -23,23 +23,18 @@ const RegisterScreen = () => {
     }
 
     try {
-      const { data } = await axios.post(
-        "/auth/register",
-        {
-          username,
-          email,
-          password,
-        }
-      );
+      const { data } = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
 
       localStorage.setItem("authToken", data.token);
 
       setTimeout(() => {
-        navigate('/');
-      }, 1800)
-
+        navigate("/");
+      }, 1800);
     } catch (error) {
-
       setError(error.response.data.error);
 
       setTimeout(() => {
@@ -49,37 +44,28 @@ const RegisterScreen = () => {
   };
 
   return (
-
     <div className="Inclusive-register-page">
-
       <div className="register-big-wrapper">
-
-
         <div className="register-banner-section ">
-
           <img src="register.png" alt="banner" width="490px" />
         </div>
 
         <div className="section-wrapper">
-
           <div className="top-suggest_login">
             <span> Have an account? </span>
             <a href="/login">Sign In</a>
           </div>
 
           <div className="top-register-explain">
-            <h2>Welcome to MERN Blog </h2>
+            <h2>Welcome to Scanner MERN Blog </h2>
 
             <p>
-              It's easy and free to post your thinking on any topic and connect with thounsands of readers.
-
+              It's easy and free to post your thinking on any topic and connect
+              with thousands of readers.
             </p>
-
-
           </div>
 
-
-          <form onSubmit={registerHandler} >
+          <form onSubmit={registerHandler}>
             {error && <div className="error_message">{error}</div>}
             <div className="input-wrapper">
               <input
@@ -91,7 +77,6 @@ const RegisterScreen = () => {
                 onChange={(e) => setUsername(e.target.value)}
               />
               <label htmlFor="name">Username</label>
-
             </div>
             <div className="input-wrapper">
               <input
@@ -104,11 +89,8 @@ const RegisterScreen = () => {
                 tabIndex={1}
               />
               <label htmlFor="email">E-mail</label>
-
-
             </div>
             <div className="input-wrapper">
-
               <input
                 type="password"
                 required
@@ -119,13 +101,9 @@ const RegisterScreen = () => {
                 value={password}
                 tabIndex={2}
               />
-              <label htmlFor="password">
-                Password
-
-              </label>
+              <label htmlFor="password">Password</label>
             </div>
             <div className="input-wrapper">
-
               <input
                 type="password"
                 required
@@ -138,16 +116,10 @@ const RegisterScreen = () => {
               <label htmlFor="confirmpassword">Confirm Password</label>
             </div>
 
-            <button type="submit" >
-              Register
-            </button>
-
+            <button type="submit">Register</button>
           </form>
-
         </div>
-
       </div>
-
     </div>
   );
 };
